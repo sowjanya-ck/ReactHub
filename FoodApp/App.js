a66ae2bd9ac0dfbcaf16cb6a809bbb72ae842c2a
sowjanya-ck <65709982+sowjanya-ck@users.noreply.github.com>
2023-12-17 15:05:46 +0000

    props

--- FoodApp/App.js
+++ FoodApp/App.js
@@ -37,33 +37,50 @@ const Header=()=>{
         </div>
     )
 }
-const ResCard =()=>{
+const ResCard =(props)=>{
+    console.log(props);
     return (
         <div className="res-card" style={{backgroundColor:"#FFFDEF"}}>
             
             <img className="reslogo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf"/>
-            <h3>McDonald's</h3>
-            <h4> Burgers, beverages, cafe</h4>
-            <h4>4.4 rating</h4>
-            <h4>30 minutes</h4>
+            <h3>{props.resName}</h3>
+            <h4> {props.cuisine}</h4>
+            <h4>{props.rate}</h4>
+            <h4>{props.time}</h4>
         </div>
     )
 
 }
+// destrucring the props, this will give the same result as above
+// const ResCard =({resName,rate,time,cuisine})=>{
+    
+//     return (
+//         <div className="res-card" style={{backgroundColor:"#FFFDEF"}}>
+            
+//             <img className="reslogo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf"/>
+//             <h3>{resName}</h3>
+//             <h4> {cuisine}</h4>
+//             <h4>{rate}</h4>
+//             <h4>{time}</h4>
+//         </div>
+//     )
+
+// }
 const Body =()=>{
     return(
         <div className="body">
             <div className="search"> Search </div>
             <div className="res-container">
-                <ResCard/>
-                <ResCard/>
-                <ResCard/>
-                <ResCard/>
-                <ResCard/>
-                <ResCard/> 
-                <ResCard/>
-                <ResCard/>
-                <ResCard/>
+                <ResCard resName="McDonald's" cuisine="burgers,cafe" rate="4.3" time="20min"/>
+                <ResCard resName="Meghana foods" cuisine="Biriyani,NorthIndian" rate="4.3" time="20min"/>
+                <ResCard resName="KFC" cuisine="burger , Fast food" rate="4.3" time="20min"/>
+                <ResCard resName="Nandana palace" cuisine="Biriyani,SouthIndian" rate="4.4" time="50min"/>
+                <ResCard resName="cafe amudhan" cuisine="Snacks,NorthIndian" rate="4.1" time="10min"/>
+                <ResCard resName="NH1 bowls" cuisine="Highway to NorthIndian" rate="4.5" time="20min"/>
+                <ResCard resName="Dominos" cuisine="Pizza,desserts" rate="4.3" time="35min"/>
+                <ResCard resName="Pizza hut" cuisine="pizza,beverages, desserts" rate="3.9" time="10min"/>
+                <ResCard resName="Nic icecreams" cuisine="ice creams,desserts" rate="4.1" time="40min"/>
+
                 
             </div>
         </div>
--- FoodApp/README.md
+++ FoodApp/README.md
@@ -1,4 +1,9 @@
 ## build food app
 
 ### 1. plan the page 
-       
+### 2. make the res-card dynamic by props.
+       -> props: passing the argument to a function.
+       -> props destructuring
+       -> config driven UI : based on the different data , UI is designed according to the data given by the COnfig.
+            -> ex: carousal is different for different city in food app.
+### 3. make the res-card dynamic by json data,  
