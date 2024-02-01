7bd8a30ac4b2868fd41438dd27d00e40c1ce473f
sowjanya-ck <65709982+sowjanya-ck@users.noreply.github.com>
2024-02-01 03:04:31 +0000

    searchbar prograss

--- FoodApp/src/components/Body.js
+++ FoodApp/src/components/Body.js
@@ -10,11 +10,15 @@ const Body =()=>{
     // now we dont need mockdata 
     const [listofRestaurants,setListofRestaurant]=useState([]);
 
+    const[searchText,setSeachText]=useState("");
+
     useEffect(()=>{
         console.log("this will render after the body component renders");
         fetchData();
 
     },[]);
+    //       whnerver a state variable gets update, react triggers a reconcilation cycle.(re-renders the componennet)
+
     console.log("first called");
     const fetchData=async ()=>{
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.3408807&lng=74.7421427&page_type=DESKTOP_WEB_LISTING");
@@ -32,9 +36,15 @@ const Body =()=>{
        
         <div className="body">
            
-            <div className="filter"> 
-                <div className="search">
-                    
+            <div className="filter" style={{display:"flex"}}> 
+                <div className="search" >
+                    <input type="text" className="search_box" value={searchText} onChange={(e)=>{setSeachText(e.target.value)}} />
+                    <button onClick={()=>{
+                        const filterSearchRestaturent= listofRestaurants.filter((res)=>{res.info.name.includes(searchText)});
+                        setListofRestaurant(filterSearchRestaturent);
+
+                        console.log(searchText)}} > search</button>
+
                 </div>
                 <button className="filter-btn" onClick={()=>{
                     console.log("clickeddd");
